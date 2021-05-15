41ec8c89e67b1d7968d7938ba31d32250aafe943
Valmisson Grizorte <missongrizorte@gmail.com>
2021-05-15 06:10:38 -0300

    feat(src-renderer): add alias of paths

--- packages/src-renderer/vite.config.js
+++ packages/src-renderer/vite.config.js
@@ -1,4 +1,12 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({})
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~': resolve(__dirname, './'),
+      '@src': resolve(__dirname, 'src')
+    }
+  }
+})
